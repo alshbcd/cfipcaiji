c80738ff6b69c6590995e61a4372636c231f12bc
alshbcd <alshbcd@163.com>
2025-05-25 13:59:02 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,7 +5,7 @@ import os
 
 # 目标URL列表
 urls = [
-        # 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
+        'https://api.uouin.com/cloudflare.html', 
         # 'https://ip.164746.xyz/ipTop.html'
         'https://ip.164746.xyz'
         ]
@@ -27,9 +27,9 @@ with open('ip.txt', 'w') as file:
         soup = BeautifulSoup(response.text, 'html.parser')
         
         # 根据网站的不同结构找到包含IP地址的元素
-        # if url == 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html':
-            # elements = soup.find_all('tr')
-        if url == 'https://ip.164746.xyz':
+        if url == 'https://api.uouin.com/cloudflare.html':
+            elements = soup.find_all('tr')
+        else url == 'https://ip.164746.xyz':
             elements = soup.find_all('tr')
         # if url == 'https://ip.164746.xyz/ipTop.html':
             # elements = soup.find_all('tr')

0ee9f24dc304433ffa5894633932ede06c021586
alshbcd <alshbcd@163.com>
2025-10-30 15:37:22 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -6,11 +6,11 @@ import os
 # 目标URL列表
 urls = [
         # 'https://api.uouin.com/cloudflare.html', 
-        # 'https://www.wetest.vip/page/cloudflare/address_v4.html',
+        'https://www.wetest.vip/page/cloudflare/address_v4.html'
         # 'https://cf.090227.xyz',
         # 'https://stock.hostmonit.com/CloudFlareYes',
         # 'https://ipdb.030101.xyz/bestcfv4/',
-        'https://ip.164746.xyz'
+        # 'https://ip.164746.xyz'
         ]
 
 # 正则表达式用于匹配IP地址
